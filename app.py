01054112104612a25af66379a9c361b609f31140
Kieran Chambers <kieran.chambers@icloud.com>
2025-01-24 16:43:08 -0500

    Fix: Import env.py at the top of file to ensure API key can be assigned to variable

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template
-import env
+import os
+import env # noqa
 
 # creating instance of Flask
 app = Flask(__name__)
 
 # loading environment variable API_KEY from env.py
-API_KEY = env.API_KEY
+API_KEY = os.getenv('API_KEY')
 
 
 # defining route for index page
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+# creatiing function to get weather data
+def get_weather_data(city):
+    import requests
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}" # noqa
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
