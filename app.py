ed4ec7d3d1522c5f5b83a8295de0827ed0ea09ee
Kieran Chambers <kieran.chambers@icloud.com>
2025-01-24 16:18:36 -0500

    Feat: Import openweathermap API key from env.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
+import env
 
+# creating instance of Flask
 app = Flask(__name__)
 
+# loading environment variable API_KEY from env.py
+API_KEY = env.API_KEY
 
+
+# defining route for index page
 @app.route("/")
 def index():
     return render_template("index.html")
